44b485d2c4234be20c3540c6afcb306599a17596
GgunHo <gh_jung@korea.ac.kr>
2026-09-02 16:53:20 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dashboard/",
-  plugins: [react()]
+  base: "/add_dashboard.github.io/",
+  plugins: [react()],
 });
